95c6f594e8609e6d3493e8d0b2e3987df9658de8
em222iv <erikper.magnusson@gmail.com>
2014-11-07 18:17:36 +0100

    timestamp fixad

--- index.php
+++ index.php
@@ -1,10 +1,31 @@
-<meta charset="utf-8">
 <?php
 
+
+$date = time();
 $data = curl_get_request('https://coursepress.lnu.se/kurser/');
 $dom = new DomDocument();
-$array = array();
-getItems($dom,$data,$array);
+$array = array("time" => $date."<br/><br/>");
+
+echo "<form method='post'>";
+echo "<input type='submit' name='submit' value='JSON-file'>";
+echo "</form>";
+echo "<form method='post'>";
+echo "<input type='submit' name='submit1' value='Scrape'>";
+echo "</form>";
+
+
+$p = json_decode(file_get_contents('filename.json'));
+if($p->{"time"} <= time() - (60*5)){
+    getItems($dom,$data,$array);
+}
+
+if(isset($_POST['submit'])) {
+    $p = json_decode(file_get_contents('filename.json'));
+    print_r($p);
+}
+if(isset($_POST['submit1'])) {
+    getItems($dom,$data,$array);
+}
 
 function getItems($dom,$data,$array) {
     $urlArray = $array;
@@ -12,17 +33,18 @@ function getItems($dom,$data,$array) {
     if($dom->loadHTML($data)){
         $xpath = new DOMXPath($dom);
         $items = $xpath->query('//ul[@id="blogs-list"]//div[@class="item-title"]/a');
+
         foreach($items as $item){
+
 
             $courseCode = getCourseCode($dom,$item->getAttribute('href'));
             $coursePlan = getCoursePlan($dom,$item->getAttribute('href'));
             $courseEntryText = getCourseEntryText($dom,$item->getAttribute('href'));
             $coursePost = getLatestPost($dom,$item->getAttribute('href'));
-            $urlArray[] = "<br/><br/> Coursename: ".$item->nodeValue . "--> Link: " . $item->getAttribute('href') . "--> Coursecode: " . $courseCode . "<br/> Courseplan: " . $coursePlan
-            . "<br/>Introduktionstext: " . $courseEntryText ."<br/> Senaste inlägget: " . $coursePost ."<br/>";
+
+            $urlArray[] = array( "Coursename: " => $item->nodeValue, "Länk: " => $item->getAttribute('href'), "Coursecode: " => $courseCode , "Courseplan: " => $coursePlan,
+            "Introduktionstext: " => $courseEntryText, "Senaste inlägget: " => $coursePost."<br/><br/>");
         }
-        $urlArray = array_map('utf8_encode', $urlArray);
-        var_dump($urlArray);
     }
     nextPage($dom,$data,$urlArray);
 }
@@ -30,24 +52,17 @@ function getItems($dom,$data,$array) {
 function getLatestPost($dom,$courseURL) {
     $courseURL = curl_get_request($courseURL);
     if($dom->loadHTML($courseURL)){
-
         $xpath = new DOMXPath($dom);
 
         $getLatestPostTitle = $xpath->query('//header[@class="entry-header"]/h1[@class="entry-title"]')->item(0);
         $firstTitle = $getLatestPostTitle->firstChild;
         $title = $firstTitle->nodeValue;
 
-
         $getLatestPostTitle = $xpath->query('//header[@class="entry-header"]/p')->item(0);
-
-
         $authorDate = $getLatestPostTitle->textContent;
-        if($title =! null){
-            return $title . " | " . $authorDate;
-        }else {
-            return "Inga inlägg";
-        }
 
+
+            return $title . " | " . $authorDate;
     }
 }
 
@@ -65,9 +80,6 @@ function getCourseEntryText($dom,$courseURL) {
         }
     }
 }
-
-
-
 
 function getCourseCode($dom,$courseURL) {
     $courseURL = curl_get_request($courseURL);
@@ -111,14 +123,21 @@ function nextPage($dom,$data,$urlArray) {
     if($dom->loadHTML($data)){
 
         $xpath = new DOMXPath($dom);
-        echo "<br/><br/><br/><br/><br/><br/><br/><br/><br/>";
+
         $nextPageUrl = $xpath->query("//div[@id='pag-bottom']/div[@class='pagination-links']/a[@class='next page-numbers']");
 
         foreach($nextPageUrl as $href){
             $nextPageUrl =  $href->getAttribute('href') . "<br/>";
         }
+        if($nextPageUrl == 1){
 
-        $nextPageUrl =  curl_get_request("https://coursepress.lnu.se" . $nextPageUrl);
+            $JSON = (object) $urlArray;
+            file_put_contents('filename.json', json_encode($JSON));
+            $JSON = json_decode(file_get_contents('filename.json'));
+            print_r($JSON);
+
+        }
+        $nextPageUrl = curl_get_request("https://coursepress.lnu.se" . $nextPageUrl);
 
         if(strlen($nextPageUrl) > 0){
             getItems($dom,$nextPageUrl,$urlArray);
